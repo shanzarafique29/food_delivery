af8efe2725432870d6b2b0d2b555e43fb9852f34
Shanza Rafique <shanzarafique290@gmail.com>
2026-08-06 13:18:10 +0500

    Added assets and design constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/onboarding.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: 
+      home:   onboardingscreen(),
     );
   }
 }
